e78a8f70f391a6cc529a09848c979315e681c6ec
pjfin <pjfin123@gmail.com>
2015-02-02 14:36:10 -0500

    Added Splash Screen and Menu

---
+++ SnowBalls 4/SnowBalls 4/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+	void OnGUI(){
+		if (GUI.Button (new Rect (Screen.width / 10, Screen.height*8 / 10, Screen.width*8 / 10, Screen.height / 10), "Play")) {
+			Application.LoadLevel (2);
+		}
+	}
+}
---
+++ SnowBalls 4/SnowBalls 4/Assets/Scripts/SplashScreenScript.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenScript : MonoBehaviour {
+	void Start () {
+		Application.LoadLevel (1);
+	}
+}
