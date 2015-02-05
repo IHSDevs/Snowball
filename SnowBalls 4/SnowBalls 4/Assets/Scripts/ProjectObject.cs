67bc70b74a843ad7156f423214c295836a8b7a3f
Julian Ohta <julianohta@gmail.com>
2015-02-05 13:56:38 -0500

    Added Files From Waypoints and Animations

---
+++ SnowBalls 4/SnowBalls 4/Assets/Scripts/ProjectObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector3 myVelocity = new Vector3 (Random.Range (-4, 4), Random.Range (-4, 4), Random.Range (-4, 4));
+		transform.rigidbody.velocity = myVelocity;
+
+		StartCoroutine("deathDelay");
+	}
+
+	IEnumerator deathDelay()
+	{
+
+		print ("jfd");
+		yield return new WaitForSeconds (1f);
+
+		print ("?");
+
+
+		this.gameObject.SetActive (false);
+		//Destroy (this);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
