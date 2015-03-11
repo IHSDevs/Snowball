f64d89cfb0d4ebbeb82a8db4204249757d4490b2
pjfin <pjfin123@gmail.com>
2015-03-11 17:30:26 -0400

    Added esc key to pause

--- SnowBalls 4/SnowBalls 4/Assets/Scripts/GUI/MenuScript.cs
+++ SnowBalls 4/SnowBalls 4/Assets/Scripts/GUI/MenuScript.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class MenuScript : MonoBehaviour {
 	void OnGUI(){
+		//Highscores Button
+		if (GUI.Button (new Rect (Screen.width / 10, Screen.height*6.5f / 10, Screen.width*8 / 10, Screen.height / 10), "Highscores")) {
+
+		}
+		//Play Button
 		if (GUI.Button (new Rect (Screen.width / 10, Screen.height*8 / 10, Screen.width*8 / 10, Screen.height / 10), "Play")) {
 			Application.LoadLevel (2);
 		}
+
+
 	}
 }
---
+++ SnowBalls 4/SnowBalls 4/Assets/Scripts/GUI/PauseScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+	bool isPaused = false;
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			isPaused = !isPaused;
+		}
+		if (isPaused) {
+			Time.timeScale = 0;
+		} else {
+			Time.timeScale = 1;
+		}
+	}
+
+}
